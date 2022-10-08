d5fd6603da63d77623e75a36ca4597ce5f7650e8
JuliaBetsenko <JuliaBetsenko@users.noreply.github.com>
2022-10-08 08:37:54 +0100

    added function formatDate

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+function formatDate(date) {
+  return `${days[dayIndex]}    ${hours}:${minutes}`;
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0 ${hours}`;
+  }
+
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${hours}`;
+  }
+  let dayIndex = currentTime.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+}
+
 dateElement = document.querySelector("#date");
 let currentTime = new Date();
-let hours = currentTime.getHours();
-if (hours < 10) {
-  hours = `0 ${hours}`;
-}
-
-let minutes = currentTime.getMinutes();
-if (minutes < 10) {
-  minutes = `0${hours}`;
-}
-let dayIndex = currentTime.getDay();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-dateElement.innerHTML = ` ${days[dayIndex]}    ${hours}:${minutes}`;
