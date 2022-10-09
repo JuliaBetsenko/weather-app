6be34482ea696d0f48dd3fd13c8a757fd8d0bcd2
JuliaBetsenko <JuliaBetsenko@users.noreply.github.com>
2022-10-09 23:35:12 +0100

    fixed city display

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function formatDate(date) {
   if (hours < 10) {
     hours = `0${hours}`;
   }
-
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -19,19 +18,16 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
   let day = days[dayIndex];
-  return `${day}    ${hours}:${minutes}`;
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-function displayWeatherCondition(response) {
-  console.log(response.data);
-}
 function search(event) {
-  let apiKey = "8944590bc97be8b65939f5eec317b745";
-  let city = document.querySelector("#city-input").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
 }
 
 function convertToFahrenheit(event) {
@@ -46,19 +42,18 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 19;
 }
 
-//challenge 1
+// Feature #1
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-//challenge 2
+// Feature #2
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-//Bonus feature
-
+// Bonus Feature
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-let celciusLink = document.querySelector("#celsius-link");
-celciusLink.addEventListener("click", convertToCelsius);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
